e408bbff6b4f58335779e7b2cb58189841325c85
lbeo <linhnh@dtn.com.vn>
2026-04-29 15:05:50 +0700

    save to database

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react'
 import Sidebar from '@/components/layout/Sidebar'
 import MainContent from '@/components/layout/MainContent'
 import PlayerBar from '@/components/player/PlayerBar'
@@ -7,8 +8,15 @@ import AudioPlayer from '@/components/player/AudioPlayer'
 import YouTubePlayer from '@/components/player/YouTubePlayer'
 import PlayerModal from '@/components/player/PlayerModal'
 import MusicPopup from '@/components/player/MusicPopup'
+import { usePlayerStore } from '@/store/playerStore'
 
 export default function HomePage() {
+  const initialize = usePlayerStore((s) => s.initialize)
+
+  useEffect(() => {
+    initialize()
+  }, [initialize])
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-1 min-h-0">
